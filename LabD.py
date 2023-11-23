b6afeb3114ea53824ba79f5b3f947189e9a7c437
semmed <semmed@ccom.unh.edu>
2023-11-23 19:03:00 +0000

    Before 2023 Lab D

---
+++ LabD.py
@@ -0,0 +1,96 @@
+## This is the template for the Lab D scipt.
+## Note that all the steps are listed here, it may be that some do not require you to code - 
+## Please do not remove any of those lines - I need them to be there.
+
+
+# D0 Create Python Script - Load the Dependecies
+
+# D01 Add the Current Folder To the Path
+
+# D02 Testing, Testing, D0.1, D0.2
+
+# D1 Defining the Vessel
+
+# D2 Representing the Transmit and Receive Array
+
+# D2.0 Representing the Transmit and Receive Array - Reality is Not Ideal
+
+# D3 Parameters for Beam Forming
+
+# D4 Get the Positioning Data
+
+# D4.0 Squaring the Positioning Data Away
+
+# D5 Get the Motion Data
+
+# D6 Get the Sound Speed Data
+
+# D7 Get the Ping Data
+
+# D7.0 Indexing the Ping Data
+
+# D8 Timing it Right
+
+# D9 Getting the Motion Data for our Epochs
+
+# D9.0 Getting Motion Vectors
+
+# D9.1 Getting Orientation Matrices
+
+# D10 Finding out Where We Are
+
+# D11 Georeferencing the Lever Arms
+
+# D12 Calculating the Reference Position RP
+
+# D13 The Separation of Shot and Receive
+
+# D14 Course Over Ground\
+
+# D15 Drift Angle
+
+# D16 Heading Change
+
+# D17 Equivalent Modeled Virtual Array
+
+# D17.0 Aligning `geo_pos_diff` to the 1-Axis
+
+# D17.1 New Transmit Transducer Lever Arm
+
+# D17.2 New Receive Transducer Lever Arm
+
+# D17.3 Virtual Transducer location: Why colocation?
+
+# D17.4 Virtual Transducer location: Vertical Placement
+
+# D18 The Intersection of Non-Orthogonal Cones
+
+# D18.0 Ideal Transducer
+
+# D18.1 Align Transducer Arrays to the Vessel Reference Frame
+
+# D18.2 Correct for Orientation at Transmit
+
+# D18.3 Correct for Orientation at Receive
+
+# D18.4 Calculate the Non-Orthogonality angle `non_ortho`
+
+# D18.5 Create a New Orthonormal Basis XYZ'
+
+# D18.6 Formula for Intersection of Orthogonal Arrays
+
+# D18.6.0 Formula for Intersection of Non-Orthogonal Arrays
+
+# D18.6.1 Formulate the Beam Vector `bv_p` in XYZ'
+
+# D18.7 Transform the Beam Vector to Geo Referenced Space
+
+# D18.8 Determine the Depression Angle of the Beam Vector
+
+# D18.9 Determine the Azimuth of the Beam Vector
+
+# D19 At last: Ray Tracing
+
+# D20 Positioning the Bottom Strike Relative to the Virtual Transducer
+
+# D21 Positioning the Bottom Strike Relative to the RP at Transmit
