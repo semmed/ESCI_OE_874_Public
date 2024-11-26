5757575c0593c997b9f547cf02c6a2394e77f3d4
semmed <semmed@ccom.unh.edu>
2024-11-26 14:16:07 +0000

    With rotation matrices added to om_math

--- mycode/om_math.py
+++ mycode/om_math.py
@@ -24,5 +24,17 @@ def incoherent_sum_log10(spl):
     # C4.8.3  Map it Back to Decibels
     return 10 * ...
         
-        
+    
+def Rx(a):
+    return np.array([[1, 0     ,  0     ],
+                     [0, cos(a), -sin(a)],
+                     [0, sin(a),  cos(a)]])
+def Ry(a):
+    return np.array([[ cos(a), 0,  sin(a)],
+                     [ 0     , 1,  0     ],
+                     [-sin(a), 0,  cos(a)]])
+def Rz(a):
+    return np.array([[cos(a), -sin(a), 0],
+                     [sin(a),  cos(a), 0],
+                     [0     ,  0     , 1]])
     
